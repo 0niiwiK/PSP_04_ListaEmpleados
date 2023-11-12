6db002fc8d9f8cc487e113c833652a252a1b36fa
Cole <israsanfuen@gmail.com>
2023-11-12 20:29:45 +0100

    Init

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,13 @@ public class Main {
             l.add(new Empleado(3, "Maria", 3000, 4000));
             l.add(new Analista(4, "Jose", 4000, 5000, 0.25, 2));
             l.imprimir();
+            l.intercambiar(1,3);
+            l.intercambiar(4,1);
+            System.out.println("");
+            l.imprimir();
+            l.sort();
+            System.out.println("");
+            l.imprimir();
         } catch (Exception e) {
             System.out.println("Otro");
         }
