ecdbf5103a8d508b83c6600c07c34262fd5c7c9e
Cole <israsanfuen@gmail.com>
2023-11-13 21:25:45 +0100

    Init

--- src/Vista/Vista.java
+++ src/Vista/Vista.java
@@ -5,16 +5,38 @@ import Controlador.Lista;
 import Modelo.Empleado;
 
 import javax.swing.*;
+import javax.swing.plaf.FileChooserUI;
+import java.io.File;
 
 public class Vista {
     private JPanel panel1;
     private JList<String> jl_lista;
     JScrollPane scrollPane;
+    private JButton btnCargar;
+    private JButton btnGuardar;
     private DefaultListModel<String> listModel;
     Lista listaempleados;
 
     public Vista() {
-
+        btnCargar.addActionListener(e -> {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            chooser.setDialogTitle("Selecciona el archivo a cargar");
+            if (chooser.showOpenDialog(panel1) == JFileChooser.APPROVE_OPTION) {
+                try {
+                    listaempleados.leerArchivo(chooser.getSelectedFile().getAbsolutePath());
+                    listModel.clear();
+                    while (!listaempleados.isLast()) {
+                        listModel.addElement( listaempleados.getAct().getNumEmp() + " " + listaempleados.getAct().getMain().getTipo());
+                        listaempleados.goNext();
+                    }
+                    listModel.addElement( listaempleados.getAct().getNumEmp() + " " + listaempleados.getAct().getMain().getTipo());
+                    jl_lista.setModel(listModel);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
 
     }
 
